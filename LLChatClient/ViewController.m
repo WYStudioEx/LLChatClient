abfe6c8270d1494a977adc29818709b8f7da947b
liguangluo <491337430@qq.com>
2017-08-22 12:43:53 +0800

    Initial Commit

---
+++ LLChatClient/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LLChatClient
+//
+//  Created by luo luo on 22/08/2017.
+//  Copyright © 2017 luo luo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ LLChatClient/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LLChatClient
+//
+//  Created by luo luo on 22/08/2017.
+//  Copyright © 2017 luo luo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
