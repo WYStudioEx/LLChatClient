d3854b1bcf283880465eab36d3a29ca7c2c1f9f6
liguangluo <491337430@qq.com>
2017-08-23 10:43:16 +0800

    上传自定义聊天客户端代码
    可注册、登录、上传头像。
    Custom chat protocol
    Registration, login, upload Avatar

---
+++ LLChatClient/ChatProtocol.h
@@ -0,0 +1,67 @@
+//
+//  ChatProtocol.h
+//  TCPClientDemo1407
+//
+//  Created by mac on 14-10-13.
+//  Copyright (c) 2014年 zhang jian. All rights reserved.
+//
+
+#ifndef TCPClientDemo1407_ChatProtocol_h
+#define TCPClientDemo1407_ChatProtocol_h
+
+//聊天客户端和服务端的通讯协议
+
+//定义请求头结构体
+typedef enum ChatRequestType {
+    ChatRequestTypeUnkonw =0,
+    ChatRequestTypeRegister=1,
+    ChatRequestTypeLogin,
+    ChatRequestTypeLogout,
+    ChatRequestTypeSendMessage,
+    ChatRequestTypeReciveMessage,
+    ChatRequestTypeUploadHeadImage,
+    ChatRequestTypeDeleteHeadImage,
+}ChatRequestType;
+
+typedef struct ChatRequestHead
+{
+    ChatRequestType type;   //请求类型
+    int subType;    //请求子类型
+    int length; //附加内容长度
+}ChatRequestHead;
+
+
+
+//响应头
+typedef struct ChatResponseHead
+{
+    int statusCode; //状态码
+    char message[128];
+}ChatResponseHead;
+
+//=========注册请求=========
+typedef struct ChatRegisterRequest
+{
+    ChatRequestHead head;
+    char username[32];
+    char password[32];
+    char email[64];
+}ChatRegisterRequest;
+
+//========登录请求=======
+typedef struct ChatLoginRequest
+{
+    ChatRequestHead head;
+    char username[32];
+    char password[32];
+}ChatLoginRequest;
+
+//========上传头像=======
+typedef struct ChatUploadHeadImageRequest
+{
+    ChatRequestHead head;
+    char username[32];
+    Byte picture[80000];
+}ChatUploadHeadImageRequest;
+
+#endif
--- LLChatClient/ViewController.m
+++ LLChatClient/ViewController.m
@@ -7,22 +7,130 @@
 //
 
 #import "ViewController.h"
+#import "AsyncSocket.h"
+#import "ChatProtocol.h"
 
-@interface ViewController ()
+@interface ViewController ()<AsyncSocketDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *userNameTF;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTF;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    //用于连接服务端的socket
+    AsyncSocket *_clientSocket;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //使用AsyncSocket和TCP协议实现客户端
+    //功能:
+    //  1.客户端连接服务端
+    //      10.3.132.7 5678
+    //  2.客户端给服务端发送消息
+    //  3.服务端接收消息
+    
+    //TCP特性
+    //1.有连接的, 发送数据之前首先连接服务器
+    //2.可靠的, 数据发送过去一般不会有数据丢失等问题
+    //3.速度相对UDP较慢
+    
+    
+    //创建
+    _clientSocket = [[AsyncSocket alloc] initWithDelegate:self];
+    
+    //TCP需要连接服务器才能发送数据
+    //注意: 不是一旦执行连接方法就连接成功的
+    NSError *error = nil;
+    // 10.3.132.7   127.0.0.1
+    [_clientSocket connectToHost:@"192.168.1.103" onPort:5678 withTimeout:-1 error:&error];
+    if(error != nil)
+    {
+        NSLog(@"连接方法执行失败");
+    }
+
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark ClickAction
+- (IBAction)loginAction:(UIButton *)sender {
+    //创建登录请求
+    ChatLoginRequest request;
+    request.head.type = ChatRequestTypeLogin;
+    strcpy(request.username, self.userNameTF.text.UTF8String);
+    strcpy(request.password, self.passwordTF.text.UTF8String);
+    
+    //发送请求
+    NSData *sendData = [[NSData alloc] initWithBytes:&request length:sizeof(ChatLoginRequest)];
+    [_clientSocket writeData:sendData withTimeout:-1 tag:100];
+}
+
+- (IBAction)RegisterAction:(UIButton *)sender {
+    //创建请求
+    ChatRegisterRequest request;
+    request.head.type = ChatRequestTypeRegister;
+    strcpy(request.username,self.userNameTF.text.UTF8String);
+    strcpy(request.password,self.passwordTF.text.UTF8String);
+    
+    //发送
+    NSData *data = [[NSData alloc] initWithBytes:&request length:sizeof(ChatRegisterRequest)];
+    [_clientSocket writeData:data withTimeout:-1 tag:100];
+}
+- (IBAction)uploadImageAction:(UIButton *)sender {
+    ChatUploadHeadImageRequest request;
+    request.head.type = ChatRequestTypeUploadHeadImage;
+    
+    NSData *data = UIImagePNGRepresentation([UIImage imageNamed:@"yiluyasha.png"]);
+    
+    
+    request.head.length = (int)data.length;
+    Byte *image = (Byte *)[data bytes];
+    strcpy(request.username,self.userNameTF.text.UTF8String);
+    memset(request.picture, 0, sizeof(request.picture));
+    memcpy(request.picture, image, data.length);
+    NSLog(@"真实长度:%d headlenght:%d",(int)data.length,request.head.length);
+    //发送
+    NSData *data2 = [[NSData alloc] initWithBytes:&request length:sizeof(ChatUploadHeadImageRequest)];
+    [_clientSocket writeData:data2 withTimeout:-1 tag:100];
+    NSLog(@"SendDataLenght:%d",(int)data2.length);
+}
+
+#pragma mark AsyncSocketDelegate
+//代理方法, 当发送完数据之后执行
+-(void)onSocket:(AsyncSocket *)sock didWriteDataWithTag:(long)tag
+{
+    //准备读取数据
+    [_clientSocket readDataWithTimeout:-1 tag:100];
+}
+//代理方法, 当接收到数据之后执行
+-(void)onSocket:(AsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag
+{
+    ChatResponseHead head;
+    [data getBytes:&head length:sizeof(ChatResponseHead)];
+    NSLog(@"code=%d message=%s",head.statusCode,head.message);
+}
+
+
+
+//代理方法, 当连接成功后执行
+-(void)onSocket:(AsyncSocket *)sock didConnectToHost:(NSString *)host port:(UInt16)port
+{
+    NSLog(@"连接成功");
+}
+
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
 }
 
 
